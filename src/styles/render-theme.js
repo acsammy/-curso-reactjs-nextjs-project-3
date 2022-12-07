af3f1a68a604f3477966a59b594b9414ae6a4bbb
Samuel Carvalho <carvalho.asamuel@gmail.com>
2022-12-07 15:43:02 -0300

    add tests in Heading

--- src/styles/render-theme.js
+++ src/styles/render-theme.js
@@ -1,3 +1,4 @@
+import { render } from '@testing-library/react';
 import { ThemeProvider } from 'styled-components';
 import { theme } from './theme';
 
--- src/templates/App/App.test.jsx
+++ src/templates/App/App.test.jsx
@@ -1,12 +1,8 @@
-import { render, screen } from '@testing-library/react';
 import { Home } from '.';
 import { renderTheme } from '../../styles/render-theme';
-import { theme } from '../../styles/theme';
 
-it('renders learn react link', () => {
-  const { debug } = renderTheme(<Home />);
-  const headingContainer = screen.findByRole('heading', {
-    name: 'Oi',
-  }).parentElement;
-  expect(headingContainer).toHaveStyleRule('backgound', 'red');
+describe('<Home />', () => {
+  it('should render home', () => {
+    renderTheme(<Home />);
+  });
 });
