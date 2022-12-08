ed3bd3616625b7d49fc2b3dfc3c7867213355c45
Samuel Carvalho <carvalho.asamuel@gmail.com>
2022-12-08 09:20:45 -0300

    add TextComponent

---
+++ src/components/TextComponent/stories.jsx
@@ -0,0 +1,20 @@
+import { TextComponent } from '.';
+
+export default {
+  title: 'TextComponent',
+  component: TextComponent,
+  args: {
+    children: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae corporis sint magni nulla alias laborum eligendi dolore, at sed veritatis velit deleniti iusto sunt laboriosam! Illum delectus quo quibusdam ea!`,
+  },
+  argTypes: {
+    children: { type: 'string' },
+  },
+};
+
+export const Template = (args) => {
+  return (
+    <div>
+      <TextComponent {...args} />
+    </div>
+  );
+};
