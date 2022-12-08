3dc3fef50abbe6d7e8d870290c67e0ec845996cb
Samuel Carvalho <carvalho.asamuel@gmail.com>
2022-12-08 09:20:52 -0300

    add MenuLink

---
+++ src/components/MenuLink/MenuLink.test.jsx
@@ -0,0 +1,31 @@
+import { screen } from '@testing-library/react';
+import { renderTheme } from '../../styles/render-theme';
+import { MenuLink } from '.';
+
+describe('<MenuLink />', () => {
+  it('should render a link', () => {
+    renderTheme(<MenuLink link="http://localhost">children</MenuLink>);
+    expect(screen.findbyRole('link', { name: 'children' })).toBeInTheDocument();
+  });
+
+  it('should open in a new tab', () => {
+    renderTheme(
+      <MenuLink link="http://localhost" newTab={true}>
+        children
+      </MenuLink>,
+    );
+    expect(screen.findbyRole('link', { name: 'children' })).toHaveAttribute(
+      'target',
+      '_blank',
+    );
+  });
+
+  it('should open in a new tab', () => {
+    renderTheme(
+      <MenuLink link="http://localhost" newTab={false}>
+        children
+      </MenuLink>,
+    );
+    expect(screen.findbyRole('link', { name: 'children' })).toMatchSnapshot();
+  });
+});
