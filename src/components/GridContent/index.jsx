3e0b69107278d46033c0690cb20608d3d3592f76
Samuel Carvalho <carvalho.asamuel@gmail.com>
2022-12-12 14:45:04 -0300

    add GridContent

---
+++ src/components/GridContent/index.jsx
@@ -0,0 +1,26 @@
+import P from 'prop-types';
+import { Heading } from '../Heading';
+import { SectionBackground } from '../SectionBackground';
+import { TextComponent } from '../TextComponent';
+import { Container, Html } from './styles';
+
+export const GridContent = ({ title, html, background = false }) => {
+  return (
+    <SectionBackground background={background}>
+      <Container>
+        <Heading uppercase darkMode={background}>
+          {title}
+        </Heading>
+        <Html>
+          <TextComponent>{html}</TextComponent>
+        </Html>
+      </Container>
+    </SectionBackground>
+  );
+};
+
+GridContent.propTypes = {
+  title: P.string.isRequired,
+  html: P.string.isRequired,
+  background: P.bool,
+};
