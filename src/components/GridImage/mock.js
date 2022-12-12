6eda3f9f872cedfa88082636839d9ccf68a36362
Samuel Carvalho <carvalho.asamuel@gmail.com>
2022-12-12 16:43:52 -0300

    add GridImage

---
+++ src/components/GridImage/mock.js
@@ -0,0 +1,47 @@
+export default {
+  background: false,
+  title: 'My Gallery',
+  description: `Porro earum eligendi animi fugiat.`,
+  grid: [
+    {
+      altText: 'Teste 1',
+      srcImg: 'https://source.unsplash.com/random/800x800/?r=1',
+    },
+    {
+      altText: 'Teste 2',
+      srcImg: 'https://source.unsplash.com/random/800x800/?r=2',
+    },
+    {
+      altText: 'Teste 3',
+      srcImg: 'https://source.unsplash.com/random/800x800/?r=3',
+    },
+    {
+      altText: 'Teste 4',
+      srcImg: 'https://source.unsplash.com/random/800x800/?r=4',
+    },
+    {
+      altText: 'Teste 5',
+      srcImg: 'https://source.unsplash.com/random/800x800/?r=5',
+    },
+    {
+      altText: 'Teste 6',
+      srcImg: 'https://source.unsplash.com/random/800x800/?r=6',
+    },
+    {
+      altText: 'Teste 7',
+      srcImg: 'https://source.unsplash.com/random/800x800/?r=7',
+    },
+    {
+      altText: 'Teste 8',
+      srcImg: 'https://source.unsplash.com/random/800x800/?r=8',
+    },
+    {
+      altText: 'Teste 9',
+      srcImg: 'https://source.unsplash.com/random/800x800/?r=9',
+    },
+    {
+      altText: 'Teste 10',
+      srcImg: 'https://source.unsplash.com/random/800x800/?r=10',
+    },
+  ],
+};
