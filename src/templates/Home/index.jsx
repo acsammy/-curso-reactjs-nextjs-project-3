adbebbffac300a714bb3a84f3125c41a32b5eb0b
Samuel Carvalho <carvalho.asamuel@gmail.com>
2022-12-21 15:27:05 -0300

    add Loading page

--- src/templates/Home/index.jsx
+++ src/templates/Home/index.jsx
@@ -3,6 +3,7 @@ import { Base } from '../Base';
 import mockBase from '../Base/mock';
 import { mapData } from '../../api/map-data';
 import { PageNotFound } from '../PageNotFound';
+import { Loading } from '../Loading';
 
 function Home() {
   const [data, setData] = useState([]);
@@ -12,7 +13,7 @@ function Home() {
     const load = async () => {
       try {
         const data = await fetch(
-          'http://localhosta:1337/api/pages/?filters[slug]=olha-so-a-minha-pagina',
+          'http://localhost:1337/api/pages/?filters[slug]=olha-so-a-minha-pagina',
         );
         const json = await data.json();
         const { attributes } = json.data[0];
@@ -37,7 +38,7 @@ function Home() {
   }
 
   if (data && !data.slug) {
-    return <h1>Carregando...</h1>;
+    return <Loading />;
   }
 
   return <Base {...mockBase} />;
